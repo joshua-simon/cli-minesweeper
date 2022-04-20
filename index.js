18ceca851e0f08399f3e1f2731af2664aad58d1f
Joshua Simon <joshy.d.simon@gmail.com>
2022-04-20 13:06:38 +1200

    started checkSquares function

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import { board,playerBoard } from './createBoard.js'
 import inquirer from 'inquirer'
 
 
-
 const assignNumbers =  () => {
   for(let i = 0; i<board.length; i++){
     for(let j=0; j<board[i].length; j++){
@@ -37,55 +36,72 @@ const assignNumbers =  () => {
 assignNumbers()
 
 
-const updatePlayerBoard = (x,y,type) => {
-  if(type === 'hit'){
-      playerBoard[x][y] = '💥'
-  }else if( type === 'number-square'){
-    playerBoard[x][y] = board[x][y].toString()
+const updatePlayerBoard = (x, y, type) => {
+  if (type === "hit") {
+    playerBoard[x][y] = "💥";
+  } else if (type === "number-square") {
+    playerBoard[x][y] = board[x][y].toString();
+    console.log(playerBoard);
+    getCoordinates();
+  } else if (type === "blank-square"){
+    playerBoard[x][y] = '⬛'
   }
-  console.log(playerBoard)
-  getCoordinates()
-}
+};
 
+const checkSquares = (x,y,board) => {
+  //check all surrounding squares
 
-
-const play = (x,y,board) => {
-  const resultType = ['hit','number-square','blank-square']
-    if(board[x][y].toString() === '💣' ){
-      updatePlayerBoard(x,y,resultType[0])
-      console.log('Game over!')
-      process.exit(1);
-    }else if(board[x][y] !== 0 && board[x][y].toString !== '💣'){
-      updatePlayerBoard(x,y,resultType[1])
+  //extract square to west
+  // console.log(board[x][y])  x,y outputs coordinates, board[x][y] outputs board element at coordinate
+  
+  if(y>0 && board[x][y].toString() !=="💣" ){
+    //NOTE: An element assigned a zero total will *never* be next to a bomb
+     const coords = [x, y-1]
+     play(coords[0],coords[1],board)
     }
+    console.log(playerBoard)
+
 }
+
+const play = (x, y, board) => {
+  const resultType = ["hit", "number-square", "blank-square"];
+  if (board[x][y].toString() === "💣") {
+    updatePlayerBoard(x, y, resultType[0]);
+    console.log("Game over!");
+    process.exit(1);
+  } else if (board[x][y] !== 0 && board[x][y].toString !== "💣") {
+    updatePlayerBoard(x, y, resultType[1]);
+  } else if(board[x][y] === 0){
+    updatePlayerBoard(x, y, resultType[2]);
+    checkSquares(x,y,board)
+  }
+};
 
 
 const getCoordinates = async () => {
-    const question = [
-        {
-          type: "input",
-          name: "x-coordinate",
-          message: "Please enter the first of two coordinates (any number from 0 up to 9)",
-        },
-        {
-          type: "input",
-          name: "y-coordinate",
-          message: "Please enter the last coordinate (any number from 0 up to 9)",
-        },
-      ];
-      let xCoordinate;
-      let yCoordinate;
+  const question = [
+    {
+      type: "input",
+      name: "x-coordinate",
+      message:
+        "Please enter the first of two coordinates (any number from 0 up to 9)",
+    },
+    {
+      type: "input",
+      name: "y-coordinate",
+      message: "Please enter the last coordinate (any number from 0 up to 9)",
+    },
+  ];
+  let xCoordinate;
+  let yCoordinate;
 
-      inquirer.prompt(question).then(answers => {
-        xCoordinate = answers["x-coordinate"];
-        yCoordinate = answers["y-coordinate"];
-      
-        play(xCoordinate,yCoordinate,board)
-      })
-      
+  inquirer.prompt(question).then((answers) => {
+    xCoordinate = answers["x-coordinate"];
+    yCoordinate = answers["y-coordinate"];
 
-}
+    play(xCoordinate, yCoordinate, board);
+  });
+};
 
 console.log(board)
 await getCoordinates()
